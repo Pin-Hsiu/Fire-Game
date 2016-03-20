b8c70e57061e11306cc1a8064c0618f2f1340029
Pin-Hsiu <j1152776@gmail.com>
2016-03-21 07:46:54 +0800

    加入人物能往某個方向走遇到牆可以旋轉一次
    還不能連續旋轉

---
+++ Assets/LineWalk.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineWalk : MonoBehaviour {
+
+	void Update () {
+		float translation = Time.deltaTime * 10;
+		transform.Translate(0, 0, translation);
+
+	}
+
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.name == "RightSide") 
+		{
+			transform.Rotate (0, -90, 0);
+		}
+	}
+
+
+
+			
+}
