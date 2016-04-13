d6c05f5a41d18f457f59335a7c708bdfcfb8036f
Pin-Hsiu <j1152776@gmail.com>
2016-04-13 15:35:55 +0800

    火燒扣血

---
+++ Assets/Blood.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blood : MonoBehaviour {
+
+	public GameObject bloodBar;//获取血条信息。  
+	public Vector3 offset;  //血条和人物坐标的差值,也就是血条在人物的上中方...之类的.这样理解
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		//让血条信息一直处于人物的头顶某处
+		bloodBar.transform.position=transform.position+offset;
+
+
+	}
+
+}
